04b8e3228eedfba2a76cb4e2b02196d5fda5ed59
Alexander Gaev <krock21rus@gmail.com>
2022-03-27 20:07:26 +0000

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -18,6 +18,6 @@ def generate_time_image_bytes(dt):
     text = convert_time_to_string(dt)
     image = get_black_background()
     font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(image, text, (int(image.shape[0]*0.1), int(image.shape[1]*0.6)), font, 4.5, (255, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(image, text, (int(image.shape[0]*0.1), int(image.shape[1]*0.6)), font, 4.5, (255, 255, 0), 10, cv2.LINE_AA)
     _, bts = cv2.imencode('.jpg', image)
     return bts.tobytes()
